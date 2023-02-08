e5496d6b21e75402d23b8186a633b02a53b0dea5
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-02-08 12:39:42 +0530

    JavaScript

---
+++ index.js
@@ -0,0 +1,11 @@
+let n = document.querySelectorAll(".drum").length;
+
+for(let i=0; i<n;i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
+        
+    });
+
+}
+
+// let audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
